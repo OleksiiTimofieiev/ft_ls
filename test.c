9d0836d4d5aff56175d7a3bcd95636cd3df11bd3
contrbas@gmail.com <K4rmU78B>
2018-10-23 21:49:00 +0300

    deleted ncurses and added some functions

--- test.c
+++ test.c
@@ -8,48 +8,83 @@
 
 typedef     struct s_temp
 {
-    int temp;
+    char *data;
     struct s_temp *next;
 }                  t_temp;
 
-void        add(t_temp **ptr)
+void add(t_temp** head_ref, char *new_data) 
+{ 
+    /* 1. allocate node */
+    t_temp* new_node = (t_temp*) malloc(sizeof(t_temp)); 
+  
+    t_temp *last = *head_ref;  /* used in step 5*/
+  
+    /* 2. put in the data  */
+    new_node->data  = strdup(new_data); 
+  
+    /* 3. This new node is going to be the last node, so make next of 
+          it as NULL*/
+    new_node->next = NULL; 
+  
+    /* 4. If the Linked List is empty, then make the new node as head */
+    if (*head_ref == NULL) 
+    { 
+       *head_ref = new_node; 
+       return; 
+    } 
+  
+    /* 5. Else traverse till the last node */
+    while (last->next != NULL) 
+        last = last->next; 
+  
+    /* 6. Change the next of last node */
+    last->next = new_node; 
+    return; 
+} 
+
+void    print_list(t_temp *tmp)
 {
-    t_temp *temp_ptr = *ptr;
-
-    if (!*ptr)
+    // printf("\n%s\n", "start");
+    while(tmp)
     {
-        *ptr = (t_temp *)malloc(sizeof(t_temp));
-        (*ptr)->temp = 1;
-        printf("(here1)\n");
+        printf("%s\n", tmp->data);
+        tmp = tmp->next;
     }
-    else
-    {
-        printf("(here2)\n");
-        while(temp_ptr->next)
-        {
-            printf("(here3)\n");
+    // printf("%s\n", "end\n");
 
-            temp_ptr = temp_ptr->next;
-        }
-        printf("(here4)\n");
-
-        temp_ptr->next = (t_temp *)malloc(sizeof(t_temp));
-        temp_ptr->temp = 2;
-
-    }
-    
 }
 
-void listdir(const char *name, int indent)
+void listdir(const char *name, int indent /* flags */)
 {
     DIR *dir;
-    struct dirent *entry;
 
+    static int x = 0;
+
+    struct dirent *entry;
 
     if (!(dir = opendir(name)))
         return;
 
     t_temp *list = NULL;
+    t_temp *list2 = NULL;
+    // error handler;
+    // id of error
+    // *S_IROTH
+
+    while ((entry = readdir(dir)) != NULL) 
+    {
+            add(&list2, entry->d_name); 
+    }
+    closedir(dir);
+    // sort(&list);
+    print_list(list2);
+
+    free(entry);
+
+    if (!(dir = opendir(name)))
+        return;
+
+    x++;
 
     while ((entry = readdir(dir)) != NULL) 
     {
@@ -61,19 +96,30 @@ void listdir(const char *name, int indent)
                 continue;
 
             snprintf(path, sizeof(path), "%s/%s", name, entry->d_name);
-            printf("%*s[%s]\n", indent, "", entry->d_name);
+            printf("%s\n", path);
+
+            // printf("%*s[%s]\n", indent, "", entry->d_name);
+
+            // add(&list, entry->d_name); 
+
             listdir(path, indent + 2);
         } 
         else 
         {
-            printf("%*s- %s\n", indent, "", entry->d_name);
-            add(&list);
+            // printf("%*s- %s\n", indent, "", entry->d_name);
+            // add(&list, entry->d_name);
         }
     }
+
     closedir(dir);
+    x--;
+
+    // if (x > 0)
+    //     print_list(list);
 }
 
-int main(void) {
+int main(void) 
+{
     listdir(".", 0);
     return 0;
 }
