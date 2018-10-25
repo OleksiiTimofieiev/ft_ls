64b2374528b4823783c41ef325a75f35e4fdb695
contrbas42 <contrbas@gmail.com>
2018-10-25 11:18:14 +0300

    little corrections

--- srcs/main.c
+++ srcs/main.c
@@ -10,33 +10,135 @@ typedef     struct s_temp
     struct s_temp *next;
 }                  t_temp;
 
+// Returns the last t_temp of the list 
+t_temp *getTail(t_temp *cur) 
+{ 
+    while (cur != NULL && cur->next != NULL) 
+        cur = cur->next; 
+    return cur; 
+} 
+  
+// Partitions the list taking the last element as the pivot 
+t_temp *partition(t_temp *head, t_temp *end, t_temp **newHead, t_temp **newEnd) 
+{ 
+    t_temp *pivot = end; 
+    t_temp *prev = NULL, *cur = head, *tail = pivot; 
+  
+    // During partition, both the head and end of the list might change 
+    // which is updated in the newHead and newEnd variables 
+    while (cur != pivot) 
+    { 
+        if (strcmp(cur->data, pivot->data) < 0) 
+        { 
+            // First t_temp that has a value less than the pivot - becomes 
+            // the new head 
+            if ((*newHead) == NULL) 
+                (*newHead) = cur; 
+  
+            prev = cur;   
+            cur = cur->next; 
+        } 
+        else // If cur t_temp is greater than pivot 
+        { 
+            // Move cur t_temp to next of tail, and change tail 
+            if (prev) 
+                prev->next = cur->next; 
+            t_temp *tmp = cur->next; 
+            cur->next = NULL; 
+            tail->next = cur; 
+            tail = cur; 
+            cur = tmp; 
+        } 
+    } 
+  
+    // If the pivot data is the smallest element in the current list, 
+    // pivot becomes the head 
+    if ((*newHead) == NULL) 
+        (*newHead) = pivot; 
+  
+    // Update newEnd to the current last t_temp 
+    (*newEnd) = tail; 
+  
+    // Return the pivot t_temp 
+    return pivot; 
+} 
+  
+  
+//here the sorting happens exclusive of the end t_temp 
+t_temp *quickSortRecur(t_temp *head, t_temp *end) 
+{ 
+    // base condition 
+    if (!head || head == end) 
+        return head; 
+  
+    t_temp *newHead = NULL, *newEnd = NULL; 
+  
+    // Partition the list, newHead and newEnd will be updated 
+    // by the partition function 
+    t_temp *pivot = partition(head, end, &newHead, &newEnd); 
+  
+    // If pivot is the smallest element - no need to recur for 
+    // the left part. 
+    if (newHead != pivot) 
+    { 
+        // Set the t_temp before the pivot t_temp as NULL 
+        t_temp *tmp = newHead; 
+        while (tmp->next != pivot) 
+            tmp = tmp->next; 
+        tmp->next = NULL; 
+  
+        // Recur for the list before pivot 
+        newHead = quickSortRecur(newHead, tmp); 
+  
+        // Change next of last t_temp of the left half to pivot 
+        tmp = getTail(newHead); 
+        tmp->next =  pivot; 
+    } 
+  
+    // Recur for the list after the pivot element 
+    pivot->next = quickSortRecur(pivot->next, newEnd); 
+  
+    return newHead; 
+} 
+  
+// The main function for quick sort. This is a wrapper over recursive 
+// function quickSortRecur() 
+// we need an additional list to save the 
+
+
+void quickSort(t_temp **headRef) 
+{ 
+    (*headRef) = quickSortRecur(*headRef, getTail(*headRef)); 
+    return; 
+} 
+
 void add(t_temp** head_ref, char *new_data) 
 { 
-    /* 1. allocate node */
-    t_temp* new_node = (t_temp*) malloc(sizeof(t_temp)); 
+    /* 1. allocate t_temp */
+    t_temp* new_t_temp = (t_temp*) malloc(sizeof(t_temp)); 
   
     t_temp *last = *head_ref;  /* used in step 5*/
   
     /* 2. put in the data  */
-    new_node->data  = strdup(new_data); 
+    new_t_temp->data  = strdup(new_data); 
   
-    /* 3. This new node is going to be the last node, so make next of 
+    /* 3. This new t_temp is going to be the last t_temp, so make next of 
           it as NULL*/
-    new_node->next = NULL; 
+    new_t_temp->next = NULL; 
   
-    /* 4. If the Linked List is empty, then make the new node as head */
+    /* 4. If the Linked List is empty, then make the new t_temp as head */
     if (*head_ref == NULL) 
     { 
-       *head_ref = new_node; 
+       *head_ref = new_t_temp; 
        return; 
     } 
   
-    /* 5. Else traverse till the last node */
+    /* 5. Else traverse till the last t_temp */
     while (last->next != NULL) 
         last = last->next; 
   
-    /* 6. Change the next of last node */
-    last->next = new_node; 
+    /* 6. Change the next of last t_temp */
+    last->next = new_t_temp; 
     return; 
 } 
 
@@ -49,10 +151,9 @@ void    print_list(t_temp *tmp)
         tmp = tmp->next;
     }
     ft_printf("\n");
-
 }
 
-void listdir(const char *name, int indent /* flags */)
+void listdir(const char *name, int indent /* flags */) // ? go from the first element of the list;
 {
     DIR *dir;
 
@@ -73,13 +174,19 @@ void listdir(const char *name, int indent /* flags */)
     {
             add(&list, entry->d_name); 
     }
+
     closedir(dir);
     // sort(&list);
+
+    // path is to be opened, not the name;
+    // path for each directory;
+
+    quickSort(&list); 
     print_list(list);
 
     free(entry);
 
-    if (!(dir = opendir(name)))
+    if (!(dir = opendir(name))) // name == path;
         return;
 
     while ((entry = readdir(dir)) != NULL) 
@@ -100,11 +207,11 @@ void listdir(const char *name, int indent /* flags */)
 
             listdir(path, indent + 2);
         } 
-        else 
-        {
-            // printf("%*s- %s\n", indent, "", entry->d_name);
-            // add(&list, entry->d_name);
-        }
+        // else 
+        // {
+        //     // printf("%*s- %s\n", indent, "", entry->d_name);
+        //     // add(&list, entry->d_name);
+        // }
     }
 
 
@@ -118,7 +225,7 @@ void listdir(const char *name, int indent /* flags */)
 int main(int argc, char **argv) 
 {
 	if (argc == 1)
-    	listdir(".", 0);
+    	listdir("./", 0);
 	else
 		ft_printf(argv[1]);
     return 0;
